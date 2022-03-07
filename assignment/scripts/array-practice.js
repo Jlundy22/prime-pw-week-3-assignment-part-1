e57918c9905074f49066598b99dd7b63bebc56f4
Jack Lund <jacklund920@gmail.com>
2022-03-07 13:16:36 -0600

    all questions complete in array-practice

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -8,7 +8,7 @@ let animalArray = ['fish', 'cat', 'bird', 'dog'];
 console.log('Animals are: ', animalArray);
 
 // 1.a. TODO: Make an array with some favorite foods
-let favoriteFoods = [' pizza', ' tacos', ' sushi'];
+let favoriteFoods = ['pizza', 'tacos', 'sushi'];
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar 
 //      to the example above
@@ -86,16 +86,21 @@ console.log(`My favorite foods are now `, favoriteFoods);
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
 //      Then log the updated array.
-
+favoriteFoods[1] = 'chicken wings';
+console.log('I changed the 2nd food on my list', favoriteFoods);
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
-
+favoriteFoods.sort();
+favoriteFoods.reverse();
+console.log('My favorite foods in reverse alphabetical order', favoriteFoods);
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
-
+console.log( favoriteFoods.join(' and '));
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+let foodAnimals = favoriteFoods.concat(animalArray);
+console.log(foodAnimals);
